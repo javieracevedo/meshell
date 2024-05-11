630b04eacab82d33a744b9c11335b79fee4d29a5
javieracevedo <javier.acevedo233@gmail.com>
2024-05-10 21:32:23 -0400

    Include necessary headers

--- include/meshellcfg.h
+++ include/meshellcfg.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+#define MAX_QUOTE_LENGTH 500
+#define MAX_QUOTE_LIST_LENGTH 50
 
 
 typedef struct {
@@ -16,3 +26,8 @@ int validate_start_sound(char* value);
 int validate_qotd_list(char* value);
 void free_cfg_items(cfgitem* items, int length);
 cfgitem* linear_search(cfgitem* items, size_t length, const char* key);
+void print_config(cfgitem* items, size_t length);
+
+
+
+
