031b1dcdfae18b00c77eba056538aa4859b8da3f
javieracevedo <javier.acevedo233@gmail.com>
2024-05-11 15:01:36 -0400

    Use fgets instead of scanf

--- src/parser.c
+++ src/parser.c
@@ -1,18 +1,34 @@
 #include "../include/parser.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+
+#define MAX_COMMAND_LINE_LENGTH 1024
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_COMMAND_ARGS_LENGTH 1024
 
 
 Command* init_command_struct() {
         Command* command = (Command*)malloc(sizeof(Command));
-        command->command = (char*)malloc(sizeof(char) * 1024);
-        command->args = (char*)malloc(sizeof(char) * 1024);
+        command->command = (char*)malloc(sizeof(char) * MAX_COMMAND_LENGTH);
+        command->args = (char*)malloc(sizeof(char) * MAX_COMMAND_ARGS_LENGTH);
 }
 
 
 Command* prompt_command() {
         printf("~/home/ ");                                      
         Command* command = init_command_struct();
-        scanf("%s", command->command);
+	char command_line[MAX_COMMAND_LINE_LENGTH];
+	char* result = fgets(command_line, MAX_COMMAND_LINE_LENGTH, stdin);
+
+	if (!result) {
+		perror("Could not read from stdin :: prompt_command");
+		exit(EXIT_FAILURE);
+	}
+	
+	command->command = strdup(result);
         return command;
 }
+
+
