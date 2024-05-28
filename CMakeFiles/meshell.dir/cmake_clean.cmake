12292976d4312ca03a07cc40494be2da9e00e3e1
javieracevedo <javier.acevedo233@gmail.com>
2024-05-28 10:57:07 -0400

    Delete old make file and configure cmakelist file

---
+++ CMakeFiles/meshell.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meshell.dir/src/main.c.o"
+  "CMakeFiles/meshell.dir/src/main.c.o.d"
+  "CMakeFiles/meshell.dir/src/parser.c.o"
+  "CMakeFiles/meshell.dir/src/parser.c.o.d"
+  "CMakeFiles/meshell.dir/src/utils.c.o"
+  "CMakeFiles/meshell.dir/src/utils.c.o.d"
+  "meshell"
+  "meshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/meshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/meshell.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for meshell.
+# This may be replaced when dependencies are built.
