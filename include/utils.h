5acc02b1af842d870f7e54c9de13cfa76c1ce2c5
javieracevedo <javier.acevedo233@gmail.com>
2024-06-03 17:16:57 -0400

    Add trim spaces util

--- include/utils.h
+++ include/utils.h
@@ -2,4 +2,4 @@ int is_str_alphanum(const char* str, int length);
 char* getstrcpy(char* str, int size);
 char* cust_strsep(char** buffer, int buffer_size, char delim);
 void cust_getline(FILE* file, char** buffer, int buffer_size);
-
+char* trim_spaces(const char *string, size_t string_length);
