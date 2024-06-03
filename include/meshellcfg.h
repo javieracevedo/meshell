cce24973e56c8a590a61fae265b34d4c8b6630af
javieracevedo <javier.acevedo233@gmail.com>
2024-06-03 17:18:02 -0400

    Add function to initialize config items (header)

--- include/meshellcfg.h
+++ include/meshellcfg.h
@@ -7,7 +7,7 @@
 
 #define true 1
 #define false 0
-#define MAX_QUOTE_LENGTH 500
+#define MAX_QUOTE_LENGTH 1024
 #define MAX_QUOTE_LIST_LENGTH 50
 
 
@@ -24,10 +24,11 @@ int validate_prompt_color(char* value);
 int validate_path(char* value);
 int validate_start_sound(char* value);
 int validate_qotd_list(char* value);
-void free_cfg_items(int length);
+void free_cfg_items();
 cfgitem* linear_search(cfgitem* items, size_t length, const char* key);
 void print_config(cfgitem* items, size_t length);
-cfgitem* get_cfg_item(char *key); 
+cfgitem* get_cfg_item(char *key);
+void init_cfgitems();
 
 
 
